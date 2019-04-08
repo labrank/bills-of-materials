0e71d3411e19eb1bff05b8c2f7feca17c660aa28
labrank <labrank@gmail.com>
2019-04-08 02:55:58 -0500

    added product managment

--- Views/view_parts.py
+++ Views/view_parts.py
@@ -4,46 +4,53 @@ from controller import Part, Parts
 
 parts = Blueprint('parts', __name__)
 
+
 @parts.route('/parts', methods=['GET'])
 def get_parts():
     resp = jsonify({"parts": Parts.all_parts})
     resp.status_code = 200
     return resp
 
+
 @parts.route('/part', methods=['POST'])
 def add_part():
     part = request.json
     elements = Parts.all_parts
-    single_part = Part(len(elements) +1, part['name'], part['childs'])
+    single_part = Part(len(elements) + 1, part['name'], part['childs'])
     Parts.addPart(single_part.__dict__)
     resp = jsonify({"parts": Parts.all_parts})
     resp.status_code = 201
     return resp
 
+
 @parts.route('/parts', methods=['POST'])
 def add_parts():
     parts = request.json['parts']
-    last_id = len(Parts.all_parts) + 1
+    last_id = len(Parts.all_parts)
     for part in parts:
         single_part = Part(last_id, part['name'], part['childs'])
-        Parts.addPart(part)
+        Parts.addPart(single_part.__dict__)
+        last_id += 1
     resp = jsonify({"parts": Parts.all_parts})
     resp.status_code = 201
     return resp
+
 
 @parts.route('/part/<string:name>', methods=['POST'])
 def add_assemblies(name):
     elements = Parts.all_parts
     parts = request.json['childs']
-    index = next((index for (index, d) in enumerate(elements) if d["name"] == name), None)
+    index = next((index for (index, d) in enumerate(
+        elements) if d["name"] == name), None)
 
     if index:
         elements[index]["childs"].extend(parts)
-        resp = jsonify({"values":elements[index]})
+        resp = jsonify({"values": elements[index]})
     else:
-        resp = jsonify({"error":"no element"})
+        resp = jsonify({"error": "no element"})
     resp.status_code = 200
     return resp
+
 
 @parts.route('/part/<string:name>', methods=['DELETE'])
 def delete_parts(name):
--- Views/view_products.py
+++ Views/view_products.py
@@ -1,8 +1,9 @@
-from flask import jsonify, request, Blueprint
+from flask import Blueprint, jsonify, request
 
 from controller import Part, Parts, Product
 
 products = Blueprint('products', __name__)
+
 
 @products.route('/products', methods=['GET'])
 def get_products():
@@ -10,44 +11,60 @@ def get_products():
     resp.status_code = 200
     return resp
 
+
 @products.route('/product/<string:name>', methods=['GET'])
 def get_product(name):
     elements = Product.all_products
-    single_product = next((item for item in elements if item['name'] == name), None)
+    single_product = next(
+        (item for item in elements if item['name'] == name), None)
     resp = jsonify({"product": single_product})
     resp.status_code = 200
     return resp
 
+
 @products.route('/product', methods=['POST'])
 def add_products():
+    # to verify if the parts exist to create a new product
     unique_products = set()
     unique_elements = set()
+    new_product = request.json
     products = Product.all_products
     parts = Parts.all_parts
-    new_products = request.json#['product']#['parts']
+
+    if any(d['product']['name'] ==
+           new_product['product']['name'] for d in products):
+        return jsonify({"error": "product already exist"})
+        
+    for prod in new_product:
+        print(prod[1])
+
+    # a set of all existing parts
     for item in parts:
         unique_products.add(item['name'])
 
-    print('-'*10)
-    def childToSet(d):
+    # Using recursion to find all parts
+    def findPartsOnProduct(d):
         if isinstance(d, dict):
-            for key, value in d.items():
+            for value in d.values():
                 if isinstance(value, (dict, list)):
-                    childToSet(value)
+                    findPartsOnProduct(value)
                 else:
                     unique_elements.add(value)
         elif isinstance(d, list):
             for item in d:
                 if isinstance(item, (dict, list)):
-                    childToSet(item)
+                    findPartsOnProduct(item)
                 unique_elements.add(item['name'])
         else:
             unique_elements.add(d['name'])
 
-    childToSet(new_products)
-    print('lista final:')
-    print(unique_elements)
-    products.append(new_products)
-
-
-    return jsonify({"products": products})
+    findPartsOnProduct(new_product)
+    # Verify if all the parts exist using sets
+    if len(unique_elements.difference(unique_products)) == 0:
+        products.append(new_product)
+        resp = jsonify({"products": products})
+        resp.status_code = 201
+    else:
+        resp = jsonify({"error": "Some parts does not exist"})
+        resp.status_code = 400
+    return resp
